5882e60e20556782879548103f1da5176a289a15
muhammed shiyahudheen <mohdshiyahudheen@gmail.com>
2023-10-12 23:01:23 +0530

    finished

--- src/Components/Stock/Stock.jsx
+++ src/Components/Stock/Stock.jsx
@@ -14,11 +14,11 @@ const Stock = () => {
     stock_name: '',
     price: '0',
   });
-  console.log(stock_id);
-  console.log(stock);
+
+  // console.log(stock);
 
   const getId = (e) => {
-    console.log(e);
+    //console.log(e);
     setStock_id(e.target.value);
     const filteredData = stock.filter((item) => item._id === e.target.value)[0];
     setPrice({
@@ -47,26 +47,22 @@ const Stock = () => {
 
   useEffect(() => {
     fetchStock();
+    console.log(stock_id);
     const socket = io('http://localhost:5000/api/socket', {
       withCredentials: true,
       auth: {
-        stock_id: stock_id, // tata id
+        stock_id: stock_id.toString(), // tata id
       },
     });
     socket.off('stock_price_update').on('stock_price_update', (data) => {
-      // console.log('final output new message', data.price);
-      const stockData = {
-        // stock_name: price.stock_name,
-        price: data.price,
-      };
-      console.log(stockData);
-      // const obj = {
-      //   ...price,
-      //   ...stockData,
-      // };
-      setPrice(stockData);
+      console.log('final output new message', data);
+      setPrice({ ...price, stock_name: data.stock_name, price: data.price });
     });
-  }, []);
+    return () => {
+      // Clean up the socket connection when the selected stock changes
+      socket.disconnect();
+    };
+  }, [stock_id]);
   return (
     <div className='stock-body'>
       <Container>
@@ -74,11 +70,13 @@ const Stock = () => {
           <Col md={12} sm={12} lg={12} className='grid-conatiner'>
             <div className='dropdown'>
               <select id='company' onChange={(e) => getId(e)}>
-                <option disabled selected>
+                <option value={setStock_id} disabled selected>
                   Select your stock
                 </option>
                 {stock.map((item) => (
-                  <option value={item._id}>{item.stock_name}</option>
+                  <option key={item._id} value={item._id}>
+                    {item.stock_name}
+                  </option>
                 ))}
               </select>
             </div>
